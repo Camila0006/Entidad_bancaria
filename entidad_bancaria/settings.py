d59a8cc7832c634632bb03657b0fa89c162cabb4
Camila0006 <camila.mendezpedraza@gmail.com>
2024-10-20 23:14:33 -0500

    Añadir dominio de Heroku a ALLOWED_HOSTS

--- entidad_bancaria/settings.py
+++ entidad_bancaria/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
